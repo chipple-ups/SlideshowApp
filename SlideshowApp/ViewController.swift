35be180bd5b2e8d38b73937ff1fbc70fd493f62f
hiroshi.okazawa <chipple_ups@yahoo.co.jp>
2021-03-22 11:57:20 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 岡澤宏 on 2021/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
